3ab96b586ab795ec69d419ada0930c8434a73f90
Sebastian Ibarguen <s@cognits.co>
2014-12-27 00:29:22 -0600

    Updated python script to also make a call to Unlock API to get location lat-long for user attributes

--- github_visualization.py
+++ github_visualization.py
@@ -2,12 +2,12 @@
 import json
 import gzip
 import urllib
-
+from unlock import Places
 
 base_url = "http://data.githubarchive.org/"
 file_directory = "data/2014-11-01-0.json.gz"
 
-filenames = ["2014-11-01-" + str(i) + ".json.gz" for i in range(1,24)]
+filenames = ["2014-11-01-" + str(i) + ".json.gz" for i in range(0,24)]
 
 accepted_event_types = ["PushEvent"]
 
@@ -17,8 +17,10 @@ def validate_input(day_of_year, event):
 def get_github_files(day_of_year, event):
     pass
 
+
 # Write to file
 with open('data/2014-11-01-PushEvent.json', 'wb') as f:
+    places = Places()
 
     # Download files
     for filename in filenames:
@@ -35,5 +37,27 @@ with open('data/2014-11-01-PushEvent.json', 'wb') as f:
                 event = json.loads(line)
 
                 if event["type"] == "PushEvent":
-                    f.write(line)
+
+                    # if user has location data, use Unlock API to get lat,long
+                    if 'actor_attributes' in event:
+                        if 'location' in event['actor_attributes']:
+                            place = event['actor_attributes']['location']
+                            print "user has location: " + place
+
+                            try:
+                                unlock_response = places.nameAndFeatureSearch( place, 'Countries', 'json' )
+                                unlock_json = json.loads(unlock_response)
+
+                                features = unlock_json['features']
+                                properties = features[0]['properties']
+                                centroid = properties['centroid']
+                                # print "got unlock lat-long: " + centroid
+
+                                event['actor_attributes']['lat'] = float(centroid.split(", ")[0])
+                                event['actor_attributes']['lon'] = float(centroid.split(", ")[-1])
+                            except:
+                                pass
+                                # print "problems with converting lat-long."
+
+                    f.write(json.dumps(event))
         print "Finished writing: " + filename
