1a974fb54639bbfdaaeb2363628d955468496d0d
Sebastian Ibarguen <s@cognits.co>
2015-01-18 19:48:40 -0600

    UPdated readme.

--- github_archive.py
+++ github_archive.py
@@ -1,7 +1,13 @@
-#
+"""
+
+To do's:
+- fix unlock data insertion. there is a problem when trying to add new data.
+- add flatten capabilities right here, so the saved file is csv.
+"""
 import json
 import gzip
 import urllib
+import csvkit
 from unlock import Places
 
 base_url = "http://data.githubarchive.org/"
@@ -38,31 +44,39 @@ with open('data/2014-11-01-PushEvent.json', 'wb') as f:
 
                 if event["type"] == "PushEvent":
 
+                    event['actor_attributes']['lat'] = "NA"
+                    event['actor_attributes']['lon'] = "NA"
+
+                    event['actor_attributes']['country'] = "NA"
+
                     # if user has location data, use Unlock API to get lat,long
                     if 'actor_attributes' in event:
                         if 'location' in event['actor_attributes']:
-                            place = event['actor_attributes']['location']
-                            print "user has location: " + place
+                            if event['actor_attributes']['location']:
 
-                            try:
-                                # unlock_response = places.nameAndFeatureSearch( place, 'Countries', 'json' )
-                                unlock_response = places.nameSearch( place, 'json' )
-                                unlock_json = json.loads(unlock_response)
+                                place = event['actor_attributes']['location']
+                                print "user has location: " + place
 
-                                features = unlock_json['features']
-                                properties = features[0]['properties']
-                                centroid = properties['centroid']
+                                try:
+                                    # unlock_response = places.nameAndFeatureSearch( place, 'Countries', 'json' )
+                                    unlock_response = places.nameSearch( place, 'json' )
+                                    unlock_json = json.loads(unlock_response)
 
-                                country = properties['country']
-                                # print "got unlock lat-long: " + centroid
+                                    features = unlock_json['features']
+                                    properties = features[0]['properties']
+                                    centroid = properties['centroid']
 
-                                event['actor_attributes']['lat'] = float(centroid.split(", ")[0])
-                                event['actor_attributes']['lon'] = float(centroid.split(", ")[-1])
+                                    country = properties['country']
+                                    # print "got unlock lat-long: " + centroid
 
-                                event['actor_attributes']['country'] = country
-                            except:
-                                pass
-                                # print "problems with converting lat-long."
+                                    event['actor_attributes']['lat'] = float(centroid.split(", ")[0])
+                                    event['actor_attributes']['lon'] = float(centroid.split(", ")[-1])
+
+                                    event['actor_attributes']['country'] = country
+                                except:
+                                    pass
+                                    # print "problems with converting lat-long."
 
                     f.write(json.dumps(event))
         print "Finished writing: " + filename
+        break
